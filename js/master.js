f05cab80b2dd0642306eb14a60f063325d00744e
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-03-20 12:07:00 +0100

    slider funzionante al click con vue

---
+++ js/master.js
@@ -0,0 +1,41 @@
+var app = new Vue(
+{
+    el : "#root",
+    data : 
+        {
+            indexImages : 0 ,
+            images : 
+            [
+                "img/71vXOkjuRZL.jpg",
+                "img/escape-from-tarkov-beta-cover.webp",
+                "img/VJh2maKk8q2XDTKSUp5aScye.webp",
+                "img/xaOrA7ZIur35LkfIzxtOECFv.webp"
+            ]
+        },
+    methods :
+        {
+            next : function()
+                    {
+                        if (this.indexImages == this.images.length -1)
+                        {
+                            this.indexImages = 0;
+                        }
+                        else 
+                        {
+                            this.indexImages++;
+                        };
+                    } ,
+
+            prew : function()
+                    {
+                        if (this.indexImages == 0)
+                        {
+                            this.indexImages = this.images.length -1;
+                        }
+                        else
+                        {
+                            this.indexImages--;
+                        };
+                    }
+        }
+});
